e6098199b1894d7a4af4b21d661cc1e2b7d4d44b
RAJAT GUPTA <105020144+RAJATGUPTA2309@users.noreply.github.com>
2024-07-23 11:40:54 +0000

    datatpyes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3)// We are using nodejs, not browser
+console.log(3 +3); console.log("hitesh")// code readability should be high
+
+
+let name = "rajat"
+let age = 26
+let isLoggedIn = false
+let state;
+
+// number => to power 53
+// bignit
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefined=> 
+// symbole => unique    
+
+
+
+
+// object
+console.log(typeof state);
